786eb1a1ae044068c0f0f14813d5945dc82049db
unknown <nataliamartineli@gmail.com>
2022-06-28 17:55:52 -0300

    Created routes with React Router

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
 import Cardapio from './pages/Cardapio';
+import Inicio from 'pages/Inicio';
+import Router from './routes';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Cardapio />
-    </React.StrictMode>,
-    document.getElementById('root')
+	<React.StrictMode>
+		<Router />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
