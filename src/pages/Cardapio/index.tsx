2f69fa517d7fb0c3a0033bb1f616b21856ca06f7
unknown <nataliamartineli@gmail.com>
2022-06-29 14:55:24 -0300

    Refatoração e criação de tema

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -4,21 +4,21 @@ import { useState } from 'react';
 import Filtros from './Filtros';
 import Ordenador from './Ordenador';
 import Itens from './Itens';
-import Menu from 'components/Menu';
+import stylesTema from 'styles/Tema.module.scss';
 
 export default function Cardapio() {
-    const [busca, setBusca] = useState('');
-    const [filtro, setFiltro] = useState<number | null>(null);
-    const [ordenador, setOrdenador] = useState('');
-    return (
-        <section className={styles.cardapio}>
-            <h3 className={styles.cardapio__titulo}>Cardápio</h3>
-            <Buscador busca={busca} setBusca={setBusca} />
-            <div className={styles.cardapio__filtros}>
-                <Filtros filtro={filtro} setFiltro={setFiltro} />
-                <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
-            </div>
-            <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
-        </section>
-    );
+	const [busca, setBusca] = useState('');
+	const [filtro, setFiltro] = useState<number | null>(null);
+	const [ordenador, setOrdenador] = useState('');
+	return (
+		<section className={styles.cardapio}>
+			<h3 className={stylesTema.titulo}>Cardápio</h3>
+			<Buscador busca={busca} setBusca={setBusca} />
+			<div className={styles.cardapio__filtros}>
+				<Filtros filtro={filtro} setFiltro={setFiltro} />
+				<Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
+			</div>
+			<Itens busca={busca} filtro={filtro} ordenador={ordenador} />
+		</section>
+	);
 }
